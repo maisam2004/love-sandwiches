091b66fee7d463603d72c6330091dd4941122f0f
maisam2004 <46633089+maisam2004@users.noreply.github.com>
2024-01-07 23:18:48 +0000

    Add module gspread and grab sheet data

---
+++ run.py
@@ -0,0 +1,19 @@
+import sys, subprocess
+subprocess.check_call([sys.executable, '-m', 'pip', 'install', 'gspread'])
+import gspread
+from google.oauth2.service_account import Credentials
+SCOPE = [
+    "https://www.googleapis.com/auth/spreadsheets",
+    "https://www.googleapis.com/auth/drive.file",
+    "https://www.googleapis.com/auth/drive"
+    ]
+CREDS = Credentials.from_service_account_file("creds.json")
+SCOPED_CREDS = CREDS.with_scopes(SCOPE)
+GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
+SHEET = GSPREAD_CLIENT.open('love_sandwiches')
+
+sales = SHEET.worksheet("sales")
+data = sales.get_all_values()
+
+print(data)
+print(data[0])

fb9330248de78c7e93b8ef3d003795564f814d1b
maisam2004 <46633089+maisam2004@users.noreply.github.com>
2024-01-08 01:30:42 +0000

    Add sales data by use update_sales_worksheet

--- run.py
+++ run.py
@@ -60,12 +60,12 @@ def update_sales_worksheet(data):
     sales_worksheet= SHEET.worksheet("sales")
     sales_worksheet.append_row(data)
     print("Sales worksheet updated successfully.\n")
-    
+
 
 
 
 data = get_sales_data()
 seles_data = [int(num) for num in data]
-
+update_sales_worksheet(seles_data)
 
 

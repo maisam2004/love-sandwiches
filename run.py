bfe99dbc75d5c707948ef2f76aed92c6633592fe
maisam2004 <46633089+maisam2004@users.noreply.github.com>
2024-01-08 01:05:42 +0000

    Adjust validation to check that all values are integers

--- run.py
+++ run.py
@@ -15,14 +15,19 @@ SHEET = GSPREAD_CLIENT.open('love_sandwiches')
 def get_sales_data():
     """
     Get sales figures input from the user
-    """
-    print("please enter sales data from the last market.")
-    print("Data should be six numbers, separated by commas.")
-    print("Example:10,20,30,40,50,60 \n")
 
-    data_str = input("Enter your data here: ")
-    sales_data = data_str.split(",")
-    validate_data(sales_data)
+    """
+    while True:
+        print("please enter sales data from the last market.")
+        print("Data should be six numbers, separated by commas.")
+        print("Example:10,20,30,40,50,60 \n")
+
+        data_str = input("Enter your data here: ")
+        sales_data = data_str.split(",")
+        if validate_data(sales_data):
+            print("Data is valid")
+            break
+    return sales_data
 
 def validate_data(values):
     """
@@ -40,6 +45,9 @@ def validate_data(values):
         
     except ValueError as e:
         print(f"Invalid data:{e.args}, please try again.\n")
+        return
+    return True
 
-get_sales_data()
+data = get_sales_data()
+
 
